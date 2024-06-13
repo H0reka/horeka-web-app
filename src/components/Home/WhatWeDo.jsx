948f1bc21d128ca2f52964b06403bc5502d3a750
RandomLorem <103768218+H4r5h-10@users.noreply.github.com>
2024-06-13 17:35:32 +0530

    Minor change to css

--- src/components/Home/WhatWeDo.jsx
+++ src/components/Home/WhatWeDo.jsx
@@ -13,7 +13,6 @@ const WhatWeDo = () => {
             title="Save Money"
             content="Order less, at bulk price. COD for extra savings."
             img="/file_2024-06-07_07.13.33.webp"
-            // img="https://assets-global.website-files.com/6489e6bc11d0980ebc5de881/65072a34fa025b719179b5d4_Frame%2037030.png"
           />
           <div className="line-con hidden lg:flex">
             <div className=" line"></div>
@@ -22,7 +21,6 @@ const WhatWeDo = () => {
             title="Save Time"
             content="Order in 2-clicks with our daily lists. OR Fix a daily quantity and forget ordering."
             img="/file_2024-06-07_07.13.21.webp"
-            // img="https://assets-global.website-files.com/6489e6bc11d0980ebc5de881/65072a4585186409b5232e9e_Frame%2037034.png"
           />
           <div className="line-con hidden lg:flex">
             <div className="line"></div>
@@ -31,7 +29,6 @@ const WhatWeDo = () => {
             title="Save your Business"
             content="Forgot to place order last night? No more Out-of-stock."
             img="/file_2024-06-07_07.12.59.webp"
-            // img="https://assets-global.website-files.com/6489e6bc11d0980ebc5de881/65072a512bef1f6db3b9d2a7_Frame%2037029.png"
           />
         </div>
       </div>

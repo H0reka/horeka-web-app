79d64425dbdfb5872c5bffd80ace46de48430bb8
RandomLorem <103768218+H4r5h-10@users.noreply.github.com>
2024-06-14 11:52:18 +0530

    Fix Bottom 'GET NOTIFIED' button

--- src/common/Bottom/Bottom.jsx
+++ src/common/Bottom/Bottom.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Bottom = () => {
-  const tab = window.location.href.split("/")[3];
-  const scroll = () =>{
+
+  const scroll = () => {
     const section = document.getElementById('banner-section');
-    section.scrollIntoView({behaviour: 'smooth'});
-  }
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div id="Banner" className="banner">
       <div className="banner-wrap">
         <div className="notice-text">
           We're launching our app very soon.{" "}
-          {tab === "" ? (
-            <span className=" cursor-pointer" onClick={()=>scroll()}>GET&nbsp;NOTIFIED</span>
-          ) : (
-            <Link to="/#banner-section" className="white-link">
+            <Link to="/#banner-section" onClick={scroll} className="white-link">
               GET&nbsp;NOTIFIED
             </Link>
-          )}
         </div>
       </div>
     </div>
